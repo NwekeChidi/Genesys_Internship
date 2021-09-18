f796062ccc2178db31f62ffb117f21ce6651e515
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-09-18 22:09:11 +0100

    first commit to functions.js

--- Week_2/functions.js
+++ Week_2/functions.js
@@ -1,41 +1,120 @@
 
-function addTask () {
-    var input = document.getElementById("input");
-    // get current text from input field
-    var newTask = input.value;
-    // only add new item to list if some text was entered 
-    if (newTask != "") {
-        // create new HTML list item
-        var item = document.createElement("li");
-        // add HTML for buttons and new task text
-        // Note, need to use '' because of "" in HTML
-        item.innerHTML = '<input type="button" class="done" onclick="markDone(this.parentNode)"  value="&#x2713;" /> ' + '<input type="button" class="remove" onclick="remove(this.parentNode)" value="&#x2715;" /> ' + '<input type="button" class="important" onclick="important(this.parentNode)" value="&#x21;" />' + newTask;
+// Create and add items to list
+function addTask(){
+    // create variables
+    var itemList = document.createElement("li");
+    var itemValue = document.getElementById("taskInput").value;
+    var textN = document.createTextNode(itemValue);
+    itemList.appendChild(textN);
+    // create an alert if user tries to add empty task
+    if (itemValue==''){
+        alert("You cannot add an empty task! Please type in a task..");
+    } else {
+        // add item to unordered list
+        document.getElementById("tasks").appendChild(itemList);
+    }
+    // clear input
+    document.getElementById("taskInput").value = "";
+    // create tagger variables
+    var taggerTrash = document.createElement("span");
+    var taggerImportant = document.createElement("span");
+    // create emojis
+    var trash = document.createTextNode("\u2715");
+    var starr = document.createTextNode("\u2606");
+    // create class names
+    taggerTrash.className = "remove";
+    taggerImportant.className = "important";
+    // append emojis to taggers
+    taggerTrash.appendChild(trash);
+    taggerImportant.appendChild(starr);
+    // add tagger to listItems
+    itemList.appendChild(taggerTrash);
+    itemList.appendChild(taggerImportant);
 
-        // add new item as part of existing list
-        document.getElementById("tasks").appendChild(item); 
+    for (i=0; i<remove.length; i++){
+        remove[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display = "none"
+        }
+    }
 
-        /* Step 4 below here */ 
-      input.value = "";
-      input.placeholder = "Enter New Task";
+    for (i=0; i<starrItem.length; i++){
+        starrItem[i].onclick = function(){
+            var div = this.parentElement;
+            if (div.style.backgroundColor != "red"){
+                div.style.backgroundColor = "red";
+                div.style.fontWeight = "bold";
+            } else {
+                div.style.backgroundColor = "";
+                div.style.fontWeight = "";
+            }
+        }
     }
 }
 
 
-function markDone (item) {
-    item.className = 'finished';
+// Create buttons and add them to item already in the list
+var tagList = document.getElementsByTagName("li");
+var counter;
+// iterating over the length of tagList
+for (counter=0; counter<tagList.length; counter++){
+    // create tagger variables
+    var taggerTrash = document.createElement("span");
+    var taggerImportant = document.createElement("span");
+    // create emojis
+    var trash = document.createTextNode("\u2715");
+    var starr = document.createTextNode("\u2606");
+    // create class names
+    taggerTrash.className = "remove";
+    taggerImportant.className = "important";
+    // append emojis to taggers
+    taggerTrash.appendChild(trash);
+    taggerImportant.appendChild(starr);
+    // add taggers to items
+    tagList[counter].appendChild(taggerTrash);
+    tagList[counter].appendChild(taggerImportant);
 }
 
 
-function remove (item) {
-    // remove item completely from document
-  if (item.className=="finished"){
-    item.remove();
-  }
-  else {
-    var txt = "Complete Task First!"
-    alert(document.getElementById("del1").innerHTML=txt);
-  }
+// to remove items from list
+var remove = document.getElementsByClassName("remove");
+// iterate through remove
+for (counter=0; counter<remove.length; counter++){
+    remove[counter].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
+
+
+// to make items important
+var starrItem = document.getElementsByClassName("important");
+// iterate through starr
+for (counter=0; counter<starrItem.length; counter++){
+    starrItem[counter].onclick = function(){
+        var div = this.parentElement;
+        if (div.style.backgroundColor != "red"){
+            div.style.backgroundColor = "red";
+            div.style.fontWeight = "bold";
+        } else {
+            div.style.backgroundColor = "";
+            div.style.fontWeight = "";
+        }
+    }
+}
+
+
+
+// to check items which have been completed
+// create a holder of queried items
+var taskList = document.querySelector("ul");
+// check completed items
+taskList.addEventListener('click', function(ev){
+    if (ev.target.tagName=='LI'){
+        ev.target.classList.toggle('check');
+    }
+}, false);
+
 
 
 function doAbout() {
@@ -44,16 +123,4 @@ function doAbout() {
   if (ddA.InnerHTML != "") {
     ddA.className = "aboutcolor"
   }
-}
-
-
-function clearAbout() {
-    var ddA = document.getElementById("divabout");
-  if (ddA.innerHTML != "") {
-    ddA.innerHTML = ""
-  }
-}
-
-function important (item) {
-    item.className = 'important';
 }
