24967167ee768d0ec96a9f75f66494eb923ed409
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-09-25 13:58:57 +0100

    Added function to calculate correlations

--- Week_3/lycanthrope_werewolf.js
+++ Week_3/lycanthrope_werewolf.js
@@ -19,11 +19,22 @@ for ( let i=0; i<eventsJournal.length; i++ ){
         countTransform += 1;
     };
 }
+// get total events
+var allEvents = [];
+for ( let i=0; i<eventsJournal.length; i++ ){
+    let currEvents = eventsJournal[i].events
+    for ( let j=0; j<currEvents.length; j++ ){
+        if ( !allEvents.includes( currEvents[j] )){
+            allEvents.push( currEvents[j] );
+        }
+    }
+}
 const lenEvents = eventsJournal.length;
 var nTransform = lenEvents - countTransform;
 var detArr =  ["Total Entries",lenEvents,
                 "Total Transformations",countTransform,
-                "Total N-Transformations", nTransform ];
+                "Total N-Transformations",nTransform,
+                "Total Events",allEvents.length ];
 
 for ( let i=0; i<detArr.length; i++ ){
     console.log( detArr[i], " : ", detArr[i+1]  );
@@ -65,7 +76,51 @@ function createTable( event, eventArr ){
     }
     return table;
 }
-
-// test createTable function
-console.log(  createTable( "carrot", eventsJournal ) );
+//// test createTable function (uncomment the lines below to run)
+console.log(  createTable( "beer", eventsJournal ) );
 console.log(  createTable( "meat", eventsJournal ) );
+console.log(  createTable( "full moon", eventsJournal ) );
+console.log(  createTable( "angry", eventsJournal ) );
+
+// Step 2: Create a function to calculate the correlation between each event and the main event
+function calculateCorr( tableArr ){
+    /// Using the relation: (d*a - c*b) / sqrt(e*f*g*h)
+    ///for clarity on relation: goto-http//slides.com/dmccraw/eloquent-javascript-chapter-4-part-2/fullscreen#/12
+    // get variables from table
+    let a = tableArr[0], b = tableArr[1];
+    let c = tableArr[2], d = tableArr[3];
+    let e = c+d, f = a+b, g = d+b, h = a+c;
+
+    let corr = ( (d*a) - (c*b) ) / ( Math.sqrt(e*f*g*h) );
+    return corr;
+}
+//// test calculateCorr function (uncomment the lines below to run)
+console.log(  calculateCorr( createTable("happy", eventsJournal) ) );
+console.log(  calculateCorr( createTable("meat", eventsJournal) ) );
+console.log(  calculateCorr( createTable("full moon", eventsJournal) ) );
+console.log(  calculateCorr( createTable("angry", eventsJournal) ) );
+
+// Step 3: Map each correlation to a corresponding event
+// create a function to get and map all correlations to respective events
+function getAllCorrs( eventsJournal ){
+    let corrMap = {};
+    // loop through eventsJournal
+    for ( let i=0; i<eventsJournal.length; i++ ){
+        // get object of events
+        let currEvents = eventsJournal[i].events;
+        // loop through currEvents and calculate correlations
+        for ( let j=0; j<currEvents.length; j++ ){
+            // check if correlation for event has already been calculated to avoid duplicates
+            if ( !(currEvents[j] in corrMap) ){
+                corrMap[ currEvents[j] ] = calculateCorr( createTable(currEvents[j], eventsJournal) );
+            }
+        }
+    }
+    return corrMap
+}
+
+//// test getAllCorrs function (uncomment the lines below to run)
+var allCorrelations = getAllCorrs(eventsJournal)
+console.log( allCorrelations );
+console.log( allCorrelations.carrot );
+console.log( Object.keys(allCorrelations).length );
