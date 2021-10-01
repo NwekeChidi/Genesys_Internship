9cb2485f37382ba69081dc08d56f7586fc6e38ac
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-01 11:51:10 +0100

    Extracted movie data from csv file

---
+++ Week_4/extract-movie-data.js
@@ -0,0 +1,34 @@
+// This file is to run only once on a new machine
+//// Read in .csv and store as objects
+const fs = require('fs');
+const csv = require('csv-parser');
+const movieData = {"allMovies": []};
+
+// create movie data
+fs.createReadStream('./IMDB-Movie-Data.csv').pipe(csv()).on('data', function (row) {
+  const movie = {
+    id: row.Rank,
+    title: row.Title,
+    genre: row.Genre,
+    description: row.Description,
+    director: row.Director,
+    actors: row.Actors,
+    year: row.Year,
+    runtime: row.Runtime_Minutes,
+    rating: row.Rating,
+    votes: row.Votes,
+    revenue: row.Revenue_Millions,
+    metascore: row.Metascore
+  }
+  movieData.allMovies.push(movie);
+}).on('end', function () {
+  // save movieData to a JSON file
+  const jsonData = JSON.stringify(movieData);
+  // write JSON string to a file
+  fs.writeFile('./movie-data.json', jsonData, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log("JSON data is saved.");
+    });
+});
