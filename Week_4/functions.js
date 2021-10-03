bc46acff5ea622d082343217c249c5e6b4ae5ea6
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-10-03 08:51:50 +0100

    Final Commit

--- Week_4/functions.js
+++ Week_4/functions.js
@@ -1,73 +1,145 @@
-// get data to work with
-const allMovieData = require('./movie-data.json').allMovies;
-
 // prototype to convert strings to title
 String.prototype.toTitleCase = function(){
-  return this.toLowerCase().split(' ').map(function(word) {return (word.charAt(0).toUpperCase() +word.slice(1));}).join(' ');};
-
+    return this.toLowerCase().split(' ').map(function(word) {return (word.charAt(0).toUpperCase() +word.slice(1));}).join(' ');};
   
-// function to add more movies to allMovieData
-function addMovie(ctitle, cgenre, cdescription, cdirector, cactors, cyear, cruntime, crating, cvotes, crevenue, cmetascore, cnum_in_stock ){
-  let currID = allMovieData.slice(-1)[0].id;
-  // check if movie is already in data
-  for (let i=0; i<allMovieData.length; i++){
-    let currTitle = allMovieData[i].title
-    if (ctitle.toLowerCase() == currTitle.toLowerCase()){
-      console.log( `Movie with title '${ctitle}' is already in database, refer to id : '${currID}'`);
-      return;
-    }
+  class GeneralUse {
+    constructor(allMovieData, users, moviesOnRent){
+      this.allMovieData = allMovieData,
+      this.users = users,
+      this.moviesOnRent = moviesOnRent
+    };
+  
+    // function to get a movie
+    getMovie(movieName){
+      movieName = movieName.toTitleCase();
+      let currMovie = this.allMovieData.filter(movie=>movie.title===movieName)[0];
+      if (typeof currMovie == 'undefined'){
+        console.log( "No Match Found!"); return}
+      return currMovie;}
   }
-  let currMovie = {
-    id: currID+1,
-    title: ctitle.toTitleCase(),
-    genre: cgenre,
-    description: cdescription,
-    director: cdirector,
-    actors: cactors,
-    year: cyear,
-    runtime: cruntime,
-    rating: crating,
-    votes: cvotes,
-    revenue: crevenue,
-    metascore: cmetascore,
-    num_in_stock: cnum_in_stock
+  
+  class Owner extends GeneralUse {
+    // funtion to addMovie
+    addMovie(ctitle, cgenre, cdescription, cdirector, cactors, cyear, cruntime, crating, cvotes, crevenue, cmetascore, cnum_in_stock, ){
+    ctitle = ctitle.toTitleCase();
+    let newMovie = {
+      id: this.allMovieData.length+1,
+      title: ctitle,
+      genre: cgenre,
+      description: cdescription,
+      director: cdirector,
+      actors: cactors,
+      year: cyear,
+      runtime: cruntime,
+      rating: crating,
+      votes: cvotes,
+      revenue: crevenue,
+      metascore: cmetascore,
+      num_in_stock: cnum_in_stock};
+    if (this.allMovieData.length != 0){ 
+      let currID = this.allMovieData.slice(-1)[0].id;
+      // check if movie is already in data
+      let currMovie = this.allMovieData.filter(movie=>movie.title===ctitle)[0];
+      if (typeof currMovie != 'undefined'){
+        console.log( `Movie with title '${ctitle}' is already in database, refer to id : '${currID}'`);return};
+      // update movie array
+      newMovie.id = currID+1;
+      this.allMovieData.push(newMovie);return}
+    this.allMovieData.push(newMovie);}
+  
+    // function to remove movies
+    removeMovie(movieTitle){
+      movieTitle = movieTitle.toTitleCase();
+      for (let i=0; i<this.allMovieData.length; i++){
+        if (this.allMovieData[i].title == movieTitle){
+          this.allMovieData.splice(i, 1);}}}
+        
+    // function to edit movie detail
+    editMovieProp(movieTitle, prop, newValue){
+      movieTitle = movieTitle.toTitleCase();
+      let movie = this.allMovieData.filter(movie=>movie.title===movieTitle)[0];
+      if ( typeof movie != 'undefined')
+        movie[prop] = newValue;}
+  
+    // function to create user
+    createUser(firstName, lastName, address, users){
+      let lenUsers = Object.keys(this.users).length;
+      const newUser = {
+        username: firstName.toLowerCase()+0+(lenUsers+1),
+        fullName: firstName+" "+lastName,
+        address: address,
+        rentedMovies: {}}
+      this.users[newUser.username] = newUser;}
+  
+    // function to remove users
+    removeUser(username, userVar){
+      delete this.users[username]; userVar = undefined;}
+  
+    // get movies on rent
+    get onRent() {
+      return this.moviesOnRent;}
+  
+    // get a user
+    getUser(username){
+      return this.users[username]}
   };
-  // update movie array
-  allMovieData.push(currMovie);
-}
-
-
-
-// function to create user
-function createUser(firstName, lastName, address, users){
-    let lenUsers = Object.keys(users).length;
-    const newUser = {
-      username: firstName.toLowerCase()+0+(lenUsers+1),
-      firstName: firstName,
-      lastName: lastName,
-      fullName: firstName+" "+lastName,
-      address: address,
-      rentedMovies: [],
-      deleteMovie: function (movieTitle){
-        for(let i=0; i<this.rentedMovies.length; i++){
-          if(this.rentedMovies[i][0] == movieTitle){
-            rentedMovies.splice(i, 1);
-          }
-        }
-      }
+  
+  class User extends GeneralUse {
+    constructor(username, allMovieData, users, moviesOnRent){
+      super(allMovieData, users, moviesOnRent),
+      this.username = username
+      this.user = this.users[username]
     }
-    users[newUser.username] = newUser;
+  
+    // function to rent movies
+    rentMovie(movieTitle, quantity){
+      let rentedMovies = this.user.rentedMovies, newMovie = {};
+      movieTitle = movieTitle.toTitleCase();
+      let currMovie = this.allMovieData.filter(movie=>movie.title===movieTitle)[0];
+      // check if currMovie is undefined
+      if (typeof currMovie == 'undefined'){console.log("No Match Found!");return}
+      if (currMovie.title == movieTitle && currMovie.num_in_stock >= quantity){
+        if (movieTitle in rentedMovies){
+          // update users' rented movies
+          rentedMovies[movieTitle] += quantity;} else {
+            rentedMovies[movieTitle] = quantity;}
+          currMovie.num_in_stock -= quantity;
+          newMovie[movieTitle] = quantity;
+          if (this.username in this.moviesOnRent){
+            //update movies on rent
+            if (movieTitle in this.moviesOnRent[this.username]){
+              this.moviesOnRent[this.username][movieTitle] += quantity;} else {
+              this.moviesOnRent[this.username]=Object.assign(this.moviesOnRent[this.username], newMovie);
+            }} else {
+              this.moviesOnRent[this.username] = newMovie;
+            }return;}
+        if (currMovie.title == movieTitle && currMovie.num_in_stock < quantity){
+          console.log(`The movie '${movieTitle}', has only ${currMovie.num_in_stock} copies in stock currently!`); return;}
+      console.log(`'${movieTitle}' is currently out of stock!`);
+    }
+  
+    // function to return movie
+    returnMovie(movieTitle, quantity){
+      movieTitle = movieTitle.toTitleCase();
+      let currMovie = this.allMovieData.filter
+      (movie=>movie.title===movieTitle)[0];
+      let rentedMovie = this.users[this.username].rentedMovies;
+      if (typeof rentedMovie[movieTitle] != 'undefined' && rentedMovie[movieTitle] >= quantity ){
+        console.log(`You have returned '${quantity}' of the movie '${movieTitle}'!`);
+        rentedMovie[movieTitle] -= quantity;
+        this.moviesOnRent[this.username][movieTitle] -= quantity;
+        currMovie.num_in_stock += quantity
+        if (rentedMovie == quantity){
+          delete this.users[this.username].rentedMovies;}return}
+        console.log("Movie title or quantity incorrect!")}
+      
+    // function to remove movies
+    //// this function can be modified to prevent users from directly removing rented movies by creating a cart which they can manage before pushing movies to rent.
+    removeMovie(movieTitle){
+      // this function does not modify movies already on rent or the movie database, as they have not been returned
+      console.log("Use the 'returnMovie' to return movies as you are going to pay for movies which you rented and lost!")
+      movieTitle = movieTitle.toTitleCase();
+      let rentedMovies = this.users[this.username].rentedMovies;
+        delete rentedMovies[movieTitle];}
   }
-  
-  
-  // function to rent movies
-  function rentMoive(userId, )
-  
-  
-  
-  
-  
-  // export functions
-  module.exports = { 
-    addMovie,
-    createUser, };
+  module.exports = {Owner, User};
--- Week_4/index.js
+++ Week_4/index.js
@@ -2,26 +2,79 @@
 ////             Movie Lending API              ////
 ////////////////////////////////////////////////////
 // Get local functions
-const myFx = require('./functions.js');
+const allClasses = require('./functions.js');
+const User = allClasses.User, Owner = allClasses.Owner
 
-// get all movie data
-const allMovieData = require('./movie-data.json').allMovies;
-//console.log(allMovieData[0]) // take a peek at the data
+
+
+// Define variables
+const users = {}; moviesOnRent = {}; 
+var allMovieData = [];
+console.log("Movies in Store on initial store opening: ", allMovieData,"\n\n");
+// NB: For a rich movie database, uncomment the lines below and run
+allMovieData = require('./movie-data.json').allMovies;
+//console.log(allMovieData[0]); // take a peek at the data
+const owner = new Owner(allMovieData, users, moviesOnRent);
 
 // test out addMovie function
-myFx.addMovie("free Guy", "Action,Adventure", 
-"A background player in an open-world video game discovers that he is just an algorithm \
-in a loop and decides to become a hero of his own story.", "Shawn Levy",
-"Ryan Reynolds,Jodie Comer, Lil Rel Howery, Joe Keery, Taika Waititi", "2021","115", "7.4", "759101", "317.4","62",50);
-console.log(allMovieData.slice(-1)[0]);
-myFx.addMovie("free Guy", "Action,Adventure", 
-"A background player in an open-world video game discovers that he is just an algorithm in a\
- loop and decides to become a hero of his own story.","Shawn Levy", 
- "Ryan Reynolds, Jodie Comer, Lil Rel Howery, Joe Keery, Taika Waititi", "2021","115", "7.4", "759101", "317.4","62",50);
+console.log("Calling 'addMovie' API funtion which adds movies to database.......");
+owner.addMovie("free Guy", "Action,Adventure", 
+"A background player in an open-world video game discovers that he is just an algorithm in a loop and decides to become a hero of his own story.","Shawn Levy", "Ryan Reynolds, Jodie Comer, Lil Rel Howery, Joe Keery, Taika Waititi", "2021","115", "7.4", "759101", "317.4","62",50);
+owner.addMovie("bloodshot", "Action,Adventure", 
+"An elite soldier killed in a battle, is ressurected and given superhuman abilities.","David S F Wilson", "Vin Diesel, Eiza Gonzalez, Sam Heughan, Toby Kebbell, Guy Pearce", "2020","109", "5.7", "50910", "37.3",NaN,10);
+owner.addMovie("attack on titan", "Action,Adventure,Dark Fantasy,Post-Apocalyptic", 
+"A young man vows to exterminate the Titans after a Titan brings about the destruction of his hometown and the death of his mother.","Hajime Isayama", NaN, "2009-2021", "Series", NaN, NaN, NaN ,NaN, 15);
+console.log(allMovieData.slice(999, 1003), '\n\n', "Adding a movie that is already existing in database......\n");
+owner.addMovie('attack on titan');
 
-// Create users
-const users = {};
-myFx.createUser("Tony", "Stone", "400 Bentely Lane, Ohio", users);
-myFx.createUser("Bill", "Jones", "4B Lekki I, Lagos", users);
-myFx.createUser("Bola", "Akeredolu", "12 Jato Street, Lagos", users);
-console.log(users);
+console.log("\n\nCalling 'getMovie' API funtion which get a given movie from database.....");
+console.log(owner.getMovie('bloodshot'),"\n\n");
+
+console.log("Calling 'editMovieProp' API funtion which edits given properties of movies in database.....");
+owner.editMovieProp("Bloodshot", 'num_in_stock', 20);
+console.table(owner.getMovie('bloodshot'));
+
+console.log("\n\nCalling 'removeMovie' API funtion which deletes a movie from database....");
+owner.removeMovie("attack on titan");
+console.log("Trying to call 'removeMovie' API funtion on a movie deleted from database....\n");
+console.log(owner.getMovie("attack on titan"));
+
+console.log("\n\nCalling 'createUser' API funtion which creates an account for users......");
+owner.createUser("Tony", "Stone", "400 Bentely Lane, Ohio");
+owner.createUser("Bill", "Jones", "4B Lekki I, Lagos");
+owner.createUser("Bola", "Akeredolu", "12 Jato Street, Lagos");
+owner.createUser("Esy", "Bidani", "Rosy Avenue, Acrra");
+console.table(users);
+
+// instantiating a class for users
+let user1 = new User('tony01', allMovieData, users, moviesOnRent);
+const user2 = new User('bill02', allMovieData, users, moviesOnRent);
+let user3 = new User('bola03', allMovieData, users, moviesOnRent);
+const user4 = new User('esy04', allMovieData, users, moviesOnRent);
+
+//
+console.log("\n\nCalling 'removeUser' API funtion which deletes user accounts from user database....");
+owner.removeUser('bola03');
+console.table(users);
+
+//
+console.log("\n\nCalling 'rentMovie' API funtion which helps a user to rent movies from database.....");
+user1.rentMovie("bloodshot", 3); user1.rentMovie("nine lives", 14);
+user1.rentMovie("free guy", 3); user2.rentMovie("free guy", 14);
+user1.rentMovie("bloodshot", 5); user4.rentMovie("free guy", 14);
+user2.rentMovie("free guy", 5); user2.rentMovie("bloodshot", 5);
+user4.rentMovie("a monster calls", 4);
+console.table(users);
+console.log("\nTry to call 'rentMovie' on a movie not in database......\n");
+user4.rentMovie("nine lives", 4);
+
+
+console.log("\n\nCalling 'returnMovie' API funtion which helps a user to return rented movies.....\n\n");
+user1.returnMovie("bloodshot", 2);
+console.log(owner.getUser('tony01'));
+console.log("\nTrying to call 'returnMovie' with incorrect arguements.....\n");
+user1.returnMovie("bloodshot", 20);
+
+console.log("\n\nCalling 'removeMovie' API funtion which helps a user to loose rented movies.....");
+user1.removeMovie('bloodshot');
+console.log(owner.getUser('tony01'));
